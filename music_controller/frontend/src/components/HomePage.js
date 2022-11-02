3f29b29db40d0e4f026ee7d63958ac79bc9bc357
Tawfeeq Zaghlool <tawfeeq.zaghlool@gmail.com>
2022-11-02 13:21:16 +0300

    my first commit

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room"
+
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Redirect,
+  } from "react-router-dom";
+  
+  export default class HomePage extends Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return <Router>
+          <Routes>
+          <Route path="/" element={<p>This is the homePage</p>}></Route>
+          <Route path="/join" element={<RoomJoinPage/>}></Route>
+          <Route path="/create" element={<CreateRoomPage/>}></Route>
+          <Route path="/room/:roomCode" element={<Room/>}></Route>
+          </Routes>
+      </Router>
+    }
+  }
